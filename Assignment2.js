c029de59ba44f2a8897a6ef9724664428713574f
Sameer <sameersid9897@gmail.com>
2025-05-27 15:31:37 +0530

    Made modification in error files due to prev repo conflict

--- Assignment2.js
+++ Assignment2.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function Tableprint() {
     
     let a = parseInt(document.getElementById("TB1").value);
@@ -9,15 +9,4 @@ function Tableprint() {
     for (let i = 1; i <= 10; i++) 
         document.getElementById("Para").innerHTML += `${a} x ${i} = ${a * i}<br>`;
 }
-=======
-function Tableprint() {
-    
-    let a = parseInt(document.getElementById("TB1").value);
-    
-    document.getElementById("Para").innerHTML = "";
 
-
-    for (let i = 1; i <= 10; i++) 
-        document.getElementById("Para").innerHTML += `${a} x ${i} = ${a * i}<br>`;
-}
->>>>>>> 1cf99b2 (JS OBJECT DEMONSTRATION)
